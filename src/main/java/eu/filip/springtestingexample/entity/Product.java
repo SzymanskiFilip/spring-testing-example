86e9e66e803e9f74610582057756f992aef6ff3c
Filip <szymanski.fp@gmail.com>
2023-01-19 15:39:16 +0100

    Fixed hibernate overriding set ID on saving.

--- src/main/java/eu/filip/springtestingexample/entity/Product.java
+++ src/main/java/eu/filip/springtestingexample/entity/Product.java
@@ -2,16 +2,19 @@ package eu.filip.springtestingexample.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
 
-@Data
 @Entity
+@Getter
+@Setter
 @Table(name = "products")
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     private String name;
@@ -19,4 +22,20 @@ public class Product {
     private String company;
 
     private double price;
+
+    public Product() {}
+
+    public Product(UUID id, String name, String company, double price) {
+        this.id = id;
+        this.name = name;
+        this.company = company;
+        this.price = price;
+    }
+
+    public Product(String name, String company, double price) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.company = company;
+        this.price = price;
+    }
 }
